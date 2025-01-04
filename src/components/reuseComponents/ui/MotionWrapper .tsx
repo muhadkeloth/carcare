f65922404e6b565d24e488e62eea5fe1cb171e2c
muhad keloth <muhadvaram123@gmail.com>
2025-01-04 09:36:05 +0530

    Renamed folder Authenticate to authenticate

--- src/components/reuseComponents/ui/MotionWrapper .tsx
+++ src/components/reuseComponents/ui/MotionWrapper .tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Children, ReactNode } from "react";
+import { ReactNode } from "react";
 
 interface MotionWrapperProps {
     children: ReactNode; 
